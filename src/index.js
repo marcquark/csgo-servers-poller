6268beee336962b12721e6407bdd888c4ff893f2
Marc Leuser <github@mleuser.de>
2018-06-17 18:52:41 +0200

    automatic removal of unreachable servers, create new token on every query (avoids expiring token issue)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const config = require('./config')
 const loopbackClient = new LoopbackClient(config.apiBaseUrl, config.credentials.user, config.credentials.password)
 var mainLoopInterval
 var servers
+const UNREACHABLE_COUNT_BEFORE_DELETION = 100
+var unreachableServers = {}
 
 /* this function queries a single server using game-server-query */
 function queryServer(element, index, array) { // necessary prototype for forEach, we will only need the element object
@@ -21,12 +23,40 @@ function queryServer(element, index, array) { // necessary prototype for forEach
         host: ip,
         port: port
     }).then((state) => {
+        // server is up, data was received
+        if(unreachableServers.hasOwnProperty(element.id)) {
+            delete unreachableServers[element.id]
+        }
         updateServerState(element.id, true, state.name.trim(), state.map, state.players.length, state.bots.length, state.maxplayers)
     }).catch((err) => {
         if(config.logging.verbose) {
             console.log((new Date()).toISOString() + ' Query on ' + element.ipport + ' (id ' + element.id + ') failed: ' + err)
         }
-        updateServerState(element.id, false)
+
+        if(unreachableServers.hasOwnProperty(element.id)) {
+            unreachableServers[element.id]++;
+        }
+        else {
+            unreachableServers[element.id] = 0;
+        }
+
+        if(unreachableServers[element.id] > UNREACHABLE_COUNT_BEFORE_DELETION) {
+            if(config.logging.enabled) {
+                console.log((new Date()).toISOString() + ' server ' + element.id + ' (' + ipport + ') failed to be reached for ' + unreachableServers[element.id] + ' consecutive retries. deleting from database...')
+            }
+            servers.deleteById(element.id)
+                .then(function (value) {
+                    if(value.count == 1) {
+                        delete unreachableServers[element.id]
+                    }
+                })
+                .catch(function(err) {
+                    console.error((new Date()).toISOString() + ' failed to remove ' + element.id + ' from the database: ' + JSON.stringify(err))
+                })
+        }
+        else {
+            updateServerState(element.id, false)
+        }
     })
   }
   
@@ -62,35 +92,41 @@ function updateServerState(id, is_up, name, map, players, bots, players_max) {
         .catch(function (err) {
             console.error((new Date()).toISOString() + ' failed to update state for ' + id + ': ' + JSON.stringify(err))
         })
-  }
+}
 
 /* this function grabs a server list from the database and queries every single server. the query callback will then update the server's state*/
 function mainLoop() {
-    if(config.logging.enabled) {
-        console.log((new Date()).toISOString() + ' fetching server list from the api')
-    }
-    // get serverlist
-    servers.find()
-        .then(function (servers) {
-            if(config.logging.enabled) {
-                console.log((new Date()).toISOString() + ' querying ' + servers.length + ' servers')
-            }
-            servers.forEach(queryServer)
-        })
-        .catch(function (err) {
-            console.error((new Date()).toISOString() + ' could not fetch the serverlist from the api: ' + err)
-        })
-}
-
-// script starts here
-loopbackClient.createToken()
+    loopbackClient.createToken()
     .then(function() {
         servers = loopbackClient.getModel('Servers')
-        mainLoop()
-        mainLoopInterval = setInterval(mainLoop, config.pollInterval*1000)
+
+        if(config.logging.enabled) {
+            console.log((new Date()).toISOString() + ' fetching server list from the api')
+        }
+
+        if(config.logging.verbose) {
+            console.log((new Date()).toISOString() + ' unreachable server state: ' + JSON.stringify(unreachableServers))
+        }
+
+        // get serverlist
+        servers.find()
+            .then(function (servers) {
+                if(config.logging.enabled) {
+                    console.log((new Date()).toISOString() + ' querying ' + servers.length + ' servers')
+                }
+                servers.forEach(queryServer)
+            })
+            .catch(function (err) {
+                console.error((new Date()).toISOString() + ' could not fetch the serverlist from the api: ' + err)
+            })
     })
     .catch(function (err) {
         console.error((new Date()).toISOString() + ' unable to get auth token. aborting')
         console.error(err)
         process.exit(1)
     })
+}
+
+// script starts here
+mainLoop()
+mainLoopInterval = setInterval(mainLoop, config.pollInterval*1000)
